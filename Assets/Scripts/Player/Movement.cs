b7f9a95e574cec3c6821e8415afaddb3bfda568b
Flyboyace <a.c.e.brandligt@gmail.com>
2025-11-19 12:40:00 +0100

    Movement
    Movement met extra speed boost na aantal seconden rennen

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -1,64 +1,82 @@
-using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] private float movementSpeed = 5f;
-    [SerializeField] private float jumpForce = 5f;
-    [SerializeField] private float holdJumpForce = 5f;
-    [SerializeField] private float maxholdtime = 5f;
+    private float movementSpeed = 5f;
+
+    [SerializeField] private float jumpForce = 8f;
+    [SerializeField] private float holdJumpForce = 4f;
+    [SerializeField] private float maxHoldTime = 0.2f;
 
     private float jumpHoldTimer = 0f;
     private bool isJumping = false;
     private Rigidbody rb;
 
-     void Start()
+    
+    private float runTimer = 0f;
+    private float speedMultiplier = 1f;
+    private float timeToSpeedBoost = 2.5f;
+
+    private void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        
         float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
 
-        float VerticalInput = Input.GetAxis("Vertical");
+        
+        bool isMoving = horizontalInput != 0 || verticalInput != 0;
 
-        Vector3 movement = new Vector3 (horizontalInput, 0, VerticalInput) * movementSpeed;
-
-        transform.position += movement * Time.deltaTime;
-
-
-        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded()) 
+        if (isMoving)
         {
-         rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            runTimer += Time.deltaTime;
+
+            if (runTimer >= timeToSpeedBoost)
+            {
+                speedMultiplier = 2f; 
+            }
+        }
+        else
+        {
+            runTimer = 0f;
+            speedMultiplier = 1f; 
+        }
+
+        transform.position += new Vector3(
+            horizontalInput * movementSpeed * speedMultiplier * Time.deltaTime,
+            0,
+            verticalInput * movementSpeed * speedMultiplier * Time.deltaTime
+        );
+
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isJumping = true;
             jumpHoldTimer = 0f;
         }
 
         if (Input.GetKey(KeyCode.Space) && isJumping)
         {
-
-
-        if (jumpHoldTimer < maxholdtime)
-        { 
-            rb.AddForce(Vector3.up *  holdJumpForce, ForceMode.Acceleration);
-            jumpHoldTimer += Time.deltaTime;
+            if (jumpHoldTimer < maxHoldTime)
+            {
+                rb.AddForce(Vector3.up * holdJumpForce, ForceMode.Acceleration);
+                jumpHoldTimer += Time.deltaTime;
+            }
+            else
+            {
+                isJumping = false;
+            }
         }
-
-        else 
-        {
-            isJumping = !isJumping;
-        }
-
 
         if (Input.GetKeyUp(KeyCode.Space))
-        { 
-            isJumping |= !isJumping;
+        {
+            isJumping = false;
         }
 
-      }
-
+        Debug.Log(transform.position);
     }
 
     private bool IsGrounded()
