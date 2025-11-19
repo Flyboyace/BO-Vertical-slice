c582d94e18e36dcbf995a045612e4fe3656775f5
forestfox08 <j.rdb08@hotmail.com>
2025-11-19 09:27:34 +0100

    Pushing New Script
    Nieuwe Script aan het pushen voor de camera movement

--- Assets/Scripts/Player/CameraMovement.cs
+++ Assets/Scripts/Player/CameraMovement.cs
@@ -1,16 +1,16 @@
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class CameraMovement : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        // add stuff here or something
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-      // ace add details 
+        
     }
 }
