6107af4424ed986ef1a478d3237a25faa796497c
forestfox08 <j.rdb08@hotmail.com>
2025-12-16 13:01:00 +0100

    Revert "Merge branch 'Dev' of https://github.com/Flyboyace/BO-Vertical-slice into Dev"
    This reverts commit 0b8b09c03d4e22249990a29f824eef86144f9b25, reversing
    changes made to 513eaf128ad7684def73186ad27d7e0687f8aec4.

--- Assets/Scripts/Player/CameraMovement.cs
+++ Assets/Scripts/Player/CameraMovement.cs
@@ -2,66 +2,22 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    [Header("Target")]
-    public Transform target;
-
-    [Header("Positioning")]
-    public Vector3 offset = new Vector3(-50f, 67f, 67f);
-    public float smoothTime = 0.25f;
-
-    [Header("Look Ahead")]
-    public float lookAheadDistance = 2f;
-    public float lookAheadSmoothTime = 0.2f;
-
-    [Header("Bounds (Optional)")]
-    public bool useBounds = false;
-    public Vector2 minBounds;
-    public Vector2 maxBounds;
-
-    private Vector3 velocity;
-    private Vector3 lookAheadVelocity;
-    private Vector3 currentLookAhead;
-
-    private Vector3 lastTargetPosition;
+    [Header("References")]
+    [SerializeField] private Transform playerPos;
+    [Header("Attributes")]
+    [SerializeField] private bool isOnCamEdge;
 
     void Start()
     {
-        if (!target) return;
-        lastTargetPosition = target.position;
+        isOnCamEdge = false;
     }
 
-    void LateUpdate()
+    // Update is called once per frame
+    void Update()
     {
-        if (!target) return;
-
-        Vector3 movementDelta = target.position - lastTargetPosition;
-        lastTargetPosition = target.position;
-
-        Vector3 lookDir = new Vector3(movementDelta.x, 0, movementDelta.z).normalized;
-        Vector3 targetLookAhead = lookDir * lookAheadDistance;
-
-        currentLookAhead = Vector3.SmoothDamp(
-            currentLookAhead,
-            targetLookAhead,
-            ref lookAheadVelocity,
-            lookAheadSmoothTime
-        );
-        Vector3 desiredPosition = target.position + offset + currentLookAhead;
-        Vector3 smoothedPosition = Vector3.SmoothDamp(
-            transform.position,
-            desiredPosition,
-            ref velocity,
-            smoothTime
-        );
-
-        if (useBounds)
+        if (isOnCamEdge)
         {
-            smoothedPosition.x = Mathf.Clamp(smoothedPosition.x, minBounds.x, maxBounds.x);
-            smoothedPosition.z = Mathf.Clamp(smoothedPosition.z, minBounds.y, maxBounds.y);
+
         }
-
-        transform.position = smoothedPosition;
-
-        transform.LookAt(target.position + Vector3.up * 1.5f);
     }
 }
